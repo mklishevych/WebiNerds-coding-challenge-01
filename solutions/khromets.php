1f6add2f5fadf87ff48f32744974000c1abb3ca7
Alexey Khromets <akhromets@webinerds.com>
2018-08-28 17:07:37 +0300

    improve code

--- solutions/khromets.php
+++ solutions/khromets.php
@@ -1,14 +1,16 @@
 <?php
 
-$sqrt     = function($v) {return sqrt($v);};
-$halve    = function($v) {return $v / 2;};
-$plusFive = function($v) {return $v + 5;};
+$sqrt     = function ($v) {return sqrt($v);};
+$halve    = function ($v) {return $v / 2;};
+$plusFive = function ($v) {return $v + 5;};
 
-$pipe = function($num, ...$args) {
-	return array_reduce($args, function($acc, callable $func) {
-		return $func($acc);
-	}, $num);
+$pipe = function (...$args) {
+    return function ($num) use ($args) {
+        return array_reduce($args, function ($acc, callable $func) {
+            return $func($acc);
+        }, $num);
+    };
 };
 
-$suchFun = $pipe(16, $sqrt, $halve, $plusFive);
-echo $suchFun;
+$suchFun = $pipe($sqrt, $halve, $plusFive);
+echo $suchFun(16);
