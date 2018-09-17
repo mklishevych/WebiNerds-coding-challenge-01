476724e413a37e314b34e7ad17d7382df531fd96
Alexey Khromets <akhromets@webinerds.com>
2018-09-17 13:49:24 +0300

    refactor code

--- 2018-09-14/solutions/khromets.php
+++ 2018-09-14/solutions/khromets.php
@@ -1,20 +1,20 @@
 <?php
-$arr1 = [1,2,3,4,5];
-$arr2 = [2,4,8];
 
-function intersection(array $arr1, array $arr2) {
-	$lessArr = count($arr1) < count($arr2) ? $arr1 : $arr2;
-	$biggerArr = count($arr1) > count($arr2) ? $arr1 : $arr2;
+function intersection(array $arr1, array $arr2)
+{
+    $lessArr = count($arr1) < count($arr2) ? $arr1 : $arr2;
+    $biggerArr = count($arr1) > count($arr2) ? $arr1 : $arr2;
 
-	$newArr = [];
-	foreach($lessArr as $element) {
-		if(array_search($element, $biggerArr)) {
-			$newArr[] = $element;
-		}
-	}
-	
-	return $newArr;
+    $newArr = [];
+    foreach ($lessArr as $element) {
+        if (in_array($element, $biggerArr, TRUE)) {
+            $newArr[] = $element;
+        }
+    }
+
+    return $newArr;
 }
 
-$intersection = intersection($arr1, $arr2);
-var_dump($intersection);
+print_r(intersection([1,2,3,4,5], [2,4,8])); // Array ( [0] => 2 [1] => 4 )
+print_r(intersection([], [2,4,8])); // Array ( )
+print_r(intersection([1,2,3,4,5], [])); // Array ( )
